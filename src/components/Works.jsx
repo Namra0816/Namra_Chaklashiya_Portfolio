e8e85737bd90af4ad746804830f3ce8fd35b85a3
Namra0816 <namra.rc8216@gmail.com>
2024-12-19 14:25:32 -0800

    additional github link code disabled

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,7 +3,7 @@ import Tilt from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
-import { github } from "../assets";
+// import { github } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
@@ -14,7 +14,7 @@ const ProjectCard = ({
   description,
   tags,
   image,
-  source_code_link,
+  // source_code_link,
 }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
